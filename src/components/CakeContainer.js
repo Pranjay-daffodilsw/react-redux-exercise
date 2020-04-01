e543eeaa56eced65643c1eb9886b71fdc00de57f
Pranjay <Pranjay@daffodilsw.com>
2020-04-01 12:56:02 +0530

    new changes

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux'
 
-function CakeContainer(){
+
+
+function CakeContainer(props){
+    console.log(props)
     return (
         <div>
-            <h2>Number of cakes</h2>
-            <button>Buy Cake</button>
+            <h2>Number of cakes - {props.numberOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     )
 };
 
-export default CakeContainer;
+const mapStateToProps = (state) => {
+    return {
+        numberOfCakes: state.numberOfCakes
+    }
+}
+
+const mapDispachToProps = (dispach) => {
+    return {
+        buyCake: () => dispach(buyCake())
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispachToProps)(CakeContainer) ;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import cakeReducer from './cake/cakeReducer';
+import cakeReducer from './cakes/cakeReducer';
 
 const store = createStore(cakeReducer);
 
